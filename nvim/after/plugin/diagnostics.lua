641ae0edb3dfbb62901e1a7e1236286168dc25f8
Levi Butcher <levibutcher355@gmail.com>
2024-10-25 21:46:48 -0400

    Fix diagnostics

--- nvim/after/plugin/diagnostics.lua
+++ nvim/after/plugin/diagnostics.lua
@@ -1,16 +1,20 @@
 local severity = vim.diagnostic.severity
 
 vim.keymap.set("n", "]e", function()
-	vim.diagnostic.goto_next({ severity = severity.ERROR })
+    vim.diagnostic.goto_next({ severity = severity.ERROR })
 end)
 vim.keymap.set("n", "[e", function()
-	vim.diagnostic.goto_prev({ severity = severity.ERROR })
+    vim.diagnostic.goto_prev({ severity = severity.ERROR })
 end)
 
 vim.keymap.set("n", "]w", function()
-	vim.diagnostic.goto_next({ severity = severity.WARN })
+    vim.diagnostic.goto_next({ severity = severity.WARN })
 end)
 
 vim.keymap.set("n", "[w", function()
-	vim.diagnostic.goto_prev({ severity = severity.WARN })
+    vim.diagnostic.goto_prev({ severity = severity.WARN })
+end)
+
+vim.keymap.set("n", "gl", function()
+    vim.diagnostic.open_float()
 end)
