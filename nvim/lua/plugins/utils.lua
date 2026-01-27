f9c0dfbaef869bf3454e127abcb62a042fd5f705
Levi Butcher <levibutcher355@gmail.com>
2026-01-27 00:44:21 -0500

    Try out oil again

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -93,42 +93,15 @@ return {
             require("blame").setup()
         end
     },
-    -- {
-    --     'stevearc/oil.nvim',
-    --     ---@module 'oil'
-    --     ---@type oil.SetupOpts
-    --     opts = {
-    --         keymaps = {
-    --             ["C-P"] = false,
-    --             ["C-S-P"] = "actions.preview",
-    --             ["<leader>pv"] = "actions.open"
-    --         }
-    --
-    --     },
-    --     -- Optional dependencies
-    --     dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    --     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    --     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    --     lazy = false,
-    -- }
-    -- {
-    --     "kdheepak/lazygit.nvim",
-    --     lazy = true,
-    --     cmd = {
-    --         "LazyGit",
-    --         "LazyGitConfig",
-    --         "LazyGitCurrentFile",
-    --         "LazyGitFilter",
-    --         "LazyGitFilterCurrentFile",
-    --     },
-    --     -- optional for floating window border decoration
-    --     dependencies = {
-    --         "nvim-lua/plenary.nvim",
-    --     },
-    --     -- setting the keybinding for LazyGit with 'keys' is recommended in
-    --     -- order to load the plugin when the command is run for the first time
-    --     keys = {
-    --         { "<leader>gs", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    --     }
-    -- }
+    {
+      'stevearc/oil.nvim',
+      ---@module 'oil'
+      ---@type oil.SetupOpts
+      opts = {},
+      -- Optional dependencies
+      dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+      -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+      -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+      lazy = false,
+    }
 }
